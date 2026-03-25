fe38b92bce28aa802a629cd67c98c1dac48f8c64
piiivot <the_pivotsk8@hotmail.com>
2026-03-25 18:31:14 -0500

    feat: wrap root layout with Auth0Provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import '../global.css';
 import { Stack } from 'expo-router';
+import { Auth0Provider } from 'react-native-auth0';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <Auth0Provider domain={process.env.EXPO_PUBLIC_AUTH0_DOMAIN as string} clientId={process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID as string}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </Auth0Provider>
   );
 }
