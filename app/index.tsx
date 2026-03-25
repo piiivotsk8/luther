f644f328a1b17957779ec6b6d60b161cf0eec88a
piiivot <the_pivotsk8@hotmail.com>
2026-03-25 18:31:14 -0500

    feat: replace default index view with Login component

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,5 @@
-import { Text, View } from 'react-native';
+import Login from '../components/Login';
 
 export default function Index() {
-  return (
-    <View className="flex-1 justify-center items-center bg-gray-900">
-      <Text className="text-2xl font-bold text-gray-100">Luther</Text>
-    </View>
-  );
+  return <Login />;
 }
