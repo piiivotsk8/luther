a7a3be4db58920e7605d4eaee587105c880a73f2
piiivot <the_pivotsk8@hotmail.com>
2026-03-25 19:53:46 -0500

    feat: add register route to root stack layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout() {
     <Auth0Provider domain={process.env.EXPO_PUBLIC_AUTH0_DOMAIN as string} clientId={process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID as string}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="register/index" options={{ headerShown: false }} />
       </Stack>
     </Auth0Provider>
   );
