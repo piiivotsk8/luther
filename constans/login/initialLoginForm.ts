683c4d8a0b645d1603416253cc6bbb6511d1109e
piiivot <the_pivotsk8@hotmail.com>
2026-03-25 19:53:47 -0500

    feat: add initial register form constants

---
+++ constans/login/initialLoginForm.ts
@@ -0,0 +1,6 @@
+import { LoginForm } from '../../interfaces/LoginForm';
+
+export const INITIAL_LOGIN_FORM: LoginForm = {
+  email: '',
+  contrasena: '',
+};
---
+++ constans/register/initialRegisterForm.ts
@@ -0,0 +1,9 @@
+import { RegisterForm } from '../../interfaces/RegisterForm';
+
+export const INITIAL_REGISTER_FORM: RegisterForm = {
+  nombre: '',
+  email: '',
+  telefono: '',
+  contrasena: '',
+  repetirContrasena: '',
+};
